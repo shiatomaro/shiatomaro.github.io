70659378695ff7eb38a5aa013fd93231c4909141
shiatomaro <officialtrishiagavile@gmail.com>
2025-03-08 08:10:57 +0800

    changed logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,29 +12,3 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 500);
     }, 2500);
 });
-
-// Get the modal
-var popup = document.getElementById("popup");
-
-// Get the button that opens the modal
-var btn = document.getElementById("learnMoreBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close-btn")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    popup.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    popup.style.display = " none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == popup) {
-        popup.style.display = "none";
-    }
-}
