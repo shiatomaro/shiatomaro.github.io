386b12d401224cf9cf7620c273479cfc3413804b
shiatomaro <officialtrishiagavile@gmail.com>
2025-03-12 10:12:05 +0800

    changed tags and deleted js file

--- assets/js/analytics.js
+++
@@ -1,9 +0,0 @@
-fetch('/secrets.json')
-    .then(response => response.json())
-    .then(data => {
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-
-        gtag('config', data.GA_TRACKING_ID);
-    });
